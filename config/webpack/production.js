6ecc4eb6532102fd47f2e947ffeeb1c6088fd052
tostasqb <tostasqb@gmail.com>
2018-04-06 10:03:36 +0100

    prepare production compiling

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,12 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
+const merge = require('webpack-merge')
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+var sourceMapSettings = {
+  devtool: 'source-map'
+}
+
+environment.plugins.get('UglifyJs').options.uglifyOptions.ecma = 5
+
+module.exports = merge(environment.toWebpackConfig(), sourceMapSettings)
